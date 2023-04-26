78230ce9f4b4bea5266ac560c44e2d02d1207000
SaturnoRat <130088047+saturnorat@users.noreply.github.com>
2023-04-25 23:39:43 -0300

    Apresentação.java

---
+++ Apresentação.java
@@ -0,0 +1,9 @@
+public class Apresentacao {
+    public static void main(String[] args) {
+    
+        //Quem eu sou, o que faço e qual meu objetivo?
+        System.out.println("Olá, eu sou SaturnoRat!");
+        System.out.println("Eu estou aprendendo Java");
+        System.out.println("Meu objetivo é conseguir desenvolver programas e aprender a usar Java para aplicações POO, software, e no mercado de trabalho, além de conseguir utulizar em meus projetos.");
+    }
+}
