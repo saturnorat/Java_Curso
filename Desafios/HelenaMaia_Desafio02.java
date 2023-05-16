9c893a0e3670b7906d76310c9c8880481066fe1f
SaturnoRat <130088047+saturnorat@users.noreply.github.com>
2023-05-16 17:58:36 -0300

    Update HelenaMaia_Desafio02.java

--- Desafios/HelenaMaia_Desafio02.java
+++ Desafios/HelenaMaia_Desafio02.java
@@ -1,1 +1,49 @@
+/*
+ * algoritmo q fala se os alunos reprovaram ou aprovaram
+ * 16/05/2023
+ * Helena Maia
+ */
+
+ //importa o scanner
+ import java.util.Scanner;
+
+public class HelenaMaia_Desafio02{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int aprovados = 0;
+        int reprovados = 0;
+        
+        System.out.println("Digite as notas dos alunos (digite n para encerrar):");
+        
+        while (true) {
+            System.out.print("Nota 1: ");
+            double nota1 = scanner.nextDouble();
+            
+            if (nota1 < 0) {
+                break;
+            }
+            
+            System.out.print("Nota 2: ");
+            double nota2 = scanner.nextDouble();
+            
+            if (nota2 == 'n'){
+                break;
+            }
+            
+            double media = (nota1 + nota2) / 2;
+            
+            if (media > 5){
+                System.out.println("Aprovado");
+                aprovados++;
+            } else{
+                System.out.println("Reprovado");
+                reprovados++;
+            }
+        }
+        
+        System.out.println("\nQuantidade de aprovados: " + aprovados);
+        System.out.println("Quantidade de reprovados: " + reprovados);
+    }
+}
+
 
