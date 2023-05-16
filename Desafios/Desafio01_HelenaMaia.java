2faaf298a6dea0b45525ab4d09603a6e570b89bd
SaturnoRat <130088047+saturnorat@users.noreply.github.com>
2023-05-16 17:38:47 -0300

    Update Desafio01_HelenaMaia.java

--- Desafios/Desafio01_HelenaMaia.java
+++ Desafios/Desafio01_HelenaMaia.java
@@ -1,1 +1,42 @@
-a
+/*
+ * Algoritmo que determina o valor de números
+ * 16/05/2023
+ * Helena Maia
+ */
+//importa o scanner
+import java.util.Scanner;
+
+public class HelenaMaia_Desafio01{
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        //variaveis
+        int mV, n1;
+
+        //inicialização
+        mV = n1 = 0;
+        
+        //mostra pra digitar uma sequencia
+        System.out.println("Digite uma sequência de números inteiros positivos:");
+        
+        //enquanto true ("infinito") é lido n1
+        while(true){
+            n1 = leitor.nextInt();
+            
+            //SE n1 == 0 para o programa
+            if (n1 == 0){
+                break;
+            }
+            
+            //SE o numero inserido for maior, passa o valor pra variavel mV (maior valor)
+            if (n1 > mV){
+                mV = n1;
+            }
+        }
+        
+        //mostra o maior valor
+        System.out.println("O maior valor digitado foi: " + mV);
+    }
+}
+
