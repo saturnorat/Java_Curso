8461c662011c95edc5b3e7de57b16c26ff8c77a6
SaturnoRat <130088047+saturnorat@users.noreply.github.com>
2023-05-30 18:32:18 -0300

    Update HelenaMaia_Desafio02.java

--- Desafios/HelenaMaia_Desafio02.java
+++ Desafios/HelenaMaia_Desafio02.java
@@ -7,30 +7,38 @@
  //importa o scanner
  import java.util.Scanner;
 
-public class HelenaMaia_Desafio02{
+public class HelenaMaia_Desafio04{
     public static void main(String[] args) {
+     
+     //novo scanner
         Scanner scanner = new Scanner(System.in);
+     //variaveis e inicialização
         int aprovados = 0;
         int reprovados = 0;
+        double nota1, nota2, media;
+        nota1 = nota2 = media = 0.0;
+     
         
-        System.out.println("Digite as notas dos alunos (digite n para encerrar):");
+        System.out.println("Digite as notas dos alunos (digite um numero negativo para encerrar):");
         
+       //loop de ler as notas
         while (true) {
             System.out.print("Nota 1: ");
-            double nota1 = scanner.nextDouble();
+            nota1 = scanner.nextDouble();
             
+         //se a nota for negativa, para
             if (nota1 < 0) {
                 break;
             }
             
             System.out.print("Nota 2: ");
-            double nota2 = scanner.nextDouble();
+            nota2 = scanner.nextDouble();
             
-            if (nota2 == 'n'){
+            if (nota2 < 0){
                 break;
             }
-            
-            double media = (nota1 + nota2) / 2;
+           
+            media = (nota1 + nota2) / 2;
             
             if (media > 5){
                 System.out.println("Aprovado");
