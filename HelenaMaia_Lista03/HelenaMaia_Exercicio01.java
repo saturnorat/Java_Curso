f1eaaf08af53621b7885a6edc2577a9e8a382dcd
SaturnoRat <130088047+saturnorat@users.noreply.github.com>
2023-05-30 21:56:08 -0300

    Update HelenaMaia_Exercicio01.java

--- HelenaMaia_Lista03/HelenaMaia_Exercicio01.java
+++ HelenaMaia_Lista03/HelenaMaia_Exercicio01.java
@@ -1,1 +1,15 @@
-a
+/*
+ * algoritmo que mostra todos os números de 0 a 1000
+ * 15/05/2023
+ * Helena Maia
+ */
+
+public class HelenaMaia_Exercicio01{
+    public static void main(String[] args){
+
+        //cria a variavel com o valor inicial, a condição e o incremento no laço
+        for(int n = 0; n <= 1000; n++){
+            System.out.println(n); //mostra os n
+        }
+    }
+}
